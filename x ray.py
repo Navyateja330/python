e1912a7155288748beb18ed96b126acb5bab5e08
Navyateja330 <navyatejae3@gmail.com>
2025-12-26 15:47:06 +0530

    Add files via upload

---
+++ x ray.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+for i in range(n):
+    for j in range(n):
+        if (
+            j == 0 or               
+            j == n - 1 or           
+            i == 0 or             
+            i == n - 1 or           
+            i == j or               
+            j == n - i - 1        
+        ):
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()

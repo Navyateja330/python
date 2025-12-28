f483bc793007d8e0d8d58b830df10f41346f9c57
Navyateja330 <navyatejae3@gmail.com>
2025-12-28 15:00:17 +0530

    Add files via upload

--- range.py
+++ range.py
@@ -1,16 +1,8 @@
-n = int(input())
-
+n=int(input())
 for i in range(n):
-    
-    for j in range(n - i - 1):
-        print(" ", end=" ")
-
-   
-    for j in range(2 * i + 1):
-        if j == 0 or j == 2 * i or i == n - 1:
-            print("*", end=" ")
+    for j in range(n):
+        if j==0 or i==n-1 or i==j or j==n-i-1 or j==n-1:
+            print("*",end=" ")
         else:
-            print(" ", end=" ")
-
-   
-    print()
+                print(" ",end=" ")
+        print()

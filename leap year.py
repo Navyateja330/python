9d236f7a3b682c8c7604a107c7c1b6aaa261ccff
Navyateja330 <navyatejae3@gmail.com>
2025-12-28 14:56:01 +0530

    Add files via upload

---
+++ leap year.py
@@ -0,0 +1,8 @@
+year = int(input("Enter a year: "))
+
+if year % 4 == 0 and year % 100 != 0:
+    print("leap year")
+elif year % 400 == 0:
+    print("leap year")
+else:
+    print("not a leap year")
